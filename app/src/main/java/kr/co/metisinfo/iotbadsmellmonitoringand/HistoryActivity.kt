91aa7d29fdb89b84976aedf61ea4dc07fcf1f7ed
J <wognsld01@gmail.com>
2021-06-21 13:15:40 +0900

    #악취 접수 이력 조회 조건 및 이력 TITLE.

--- app/src/main/java/kr/co/metisinfo/iotbadsmellmonitoringand/HistoryActivity.kt
+++ app/src/main/java/kr/co/metisinfo/iotbadsmellmonitoringand/HistoryActivity.kt
@@ -22,15 +22,15 @@ class HistoryActivity : BaseActivity() {
 
     private lateinit var binding: ActivityHistoryBinding
 
+    /*ACTIVITY INIT*/
     override fun initLayout() {
 
-        binding = DataBindingUtil.setContentView(this, R.layout.activity_history)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_history)           //XML BIND
 
         Log.d("metis","MainActivity 시작")
         binding.includeHeader.textTitle.setText(R.string.history)                                   // 타이틀 제목
         binding.includeHeader.backButton.visibility = View.GONE                                     // 뒤로가기 버튼 안보이게
         binding.includeHeader.sideMenuButton.visibility = View.VISIBLE                              // 사이드 메뉴 버튼 보이게
-
     }
 
     override fun setOnClickListener() {
